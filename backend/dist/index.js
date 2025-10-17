f36152a14e48fd9fd1d9c196d8d47adfa62f673a
Anuj Bajpai <anujbajpai.1212@gmail.com>
2025-10-17 20:07:05 +0000

    added offer, answer and ice-candidates

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,11 +1,37 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
+let senderSocket = null;
+let receiverSocket = null;
 wss.on('connection', function connection(ws) {
-    console.log('New client connected');
+    ws.on('error', console.error);
     ws.on('message', function message(data) {
         const message = JSON.parse(data.toString());
-        console.log('Received message:', message);
+        if (message.type === 'sender') {
+            senderSocket = ws;
+        }
+        else if (message.type === 'receiver') {
+            receiverSocket = ws;
+        }
+        else if (message.type === 'createOffer') {
+            if (ws !== senderSocket) {
+                return;
+            }
+            receiverSocket?.send(JSON.stringify({ type: 'createOffer', sdp: message.sdp }));
+        }
+        else if (message.type === 'createAnswer') {
+            if (ws !== receiverSocket) {
+                return;
+            }
+            senderSocket?.send(JSON.stringify({ type: 'createAnswer', sdp: message.sdp }));
+        }
+        else if (message.type === 'iceCandidate') {
+            if (ws === senderSocket) {
+                receiverSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+            }
+            else if (ws === receiverSocket) {
+                senderSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+            }
+        }
     });
-    ws.send('Welcome to the WebSocket server!');
 });
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,35 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-    console.log('New client connected');
+let senderSocket: null | WebSocket = null;
+let receiverSocket: null | WebSocket = null;
+
+wss.on('connection', function connection(ws: WebSocket) {
+    ws.on('error', console.error);
 
     ws.on('message', function message(data: Buffer) {
         const message = JSON.parse(data.toString());
-        console.log('Received message:', message);
+        if (message.type === 'sender') {
+            senderSocket = ws;
+        } else if (message.type === 'receiver') {
+            receiverSocket = ws;
+        } else if (message.type === 'createOffer') {
+            if (ws !== senderSocket) {
+                return;
+            }
+            receiverSocket?.send(JSON.stringify({ type: 'createOffer', sdp: message.sdp }));
+        } else if (message.type === 'createAnswer') {
+            if (ws !== receiverSocket) {
+                return;
+            }
+            senderSocket?.send(JSON.stringify({ type: 'createAnswer', sdp: message.sdp }));
+        } else if (message.type === 'iceCandidate') {
+            if (ws === senderSocket) {
+                receiverSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+            } else if (ws === receiverSocket) {
+                senderSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+            }
+        }
     });
-
-    ws.send('Welcome to the WebSocket server!');
 });
-
